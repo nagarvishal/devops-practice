9f47fecb817ec560c2215e87101c31c423f72ef3
Vishal Nagar <vishalnagar@Vishals-MacBook-Air.local>
2025-10-31 18:34:52 +0530

    Testing

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,8 +6,3 @@ cd /home/ubuntu/my-node-app
 echo "Starting app with PM2..."
 pm2 start index.js --name "my-node-app"
 
-echo "Saving PM2 process list..."
-pm2 save
-
-echo "Restarting PM2 service..."
-pm2 startup systemd -u ubuntu --hp /home/ubuntu
